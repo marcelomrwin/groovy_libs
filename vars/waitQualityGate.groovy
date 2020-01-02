9b1e769f4a53295b520251c8e53b76f347116230
Marcelo Daniel Silva Sales <marcelo.d.sales@gmail.com>
2020-01-02 15:04:30 -0300

    Update waitQualityGate.groovy

--- vars/waitQualityGate.groovy
+++ vars/waitQualityGate.groovy
@@ -1,10 +1,11 @@
 #!/usr/bin/env groovy
 
 def call() {
-    def groupId    = getGroupIdFromPom()
-    def artifactId = getArtifactIdFromPom()
+    def groupId    = this.env.GROUP_ID
+    def artifactId = this.env.ARTIFACT_ID
+    def branch     = this.env.BRANCH_NAME
     def sonar      = getSonarQubeURL()
-    def api = "${sonar}/api/qualitygates/project_status?projectKey=${groupId}:${artifactId}"
+    def api = "${sonar}/api/qualitygates/project_status?projectKey=${groupId}-${artifactId}-${branch}"
     def url = new URL(api)
     def result = new groovy.json.JsonSlurper().parse(url.newReader())
     result.projectStatus
